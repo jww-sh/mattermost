d1b788d473114b812eced27e905e56b3d0663fcf
Jared Wright <jared.wright@platform.sh>
2026-02-19 15:41:28 +0200

    upsun

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export MATTERMOST_VERSION=$(cat mattermost_version)
 
 download_mattermost() {
     printf "\n  ✔ \033[1mDownloading Mattermost...\033[0m ($MATTERMOST_VERSION)\n\n"
-    wget --quiet -c https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz -O - | tar -xz
+    wget --quiet -c https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-team-${MATTERMOST_VERSION}-linux-amd64.tar.gz -O - | tar -xz
     cp -a mattermost/* .
     chmod +x bin/mattermost
 }
