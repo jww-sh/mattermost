6ee7f5a1b53b4217a108765880221f98b0e8ab96
Jared Wright <jared.wright@platform.sh>
2026-02-19 16:14:38 +0200

    upsun

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,6 @@ set_local(){
 }
 
 first_deploy() {
-
-    # Demo details - currently blocked by upstream issue (version mismatch - https://github.com/mattermost/mattermost-server/issues/19023).
     printf "\n\033[1mInitializing Mattermost on first deploy...\033[0m\n"
     printf "\n  ✔ \033[1mCreating initial admin user\033[0m ($PSH_INITADMIN_USERNAME/$PSH_INITADMIN_EMAIL/$PSH_INITADMIN_PASSWORD)\n      "
     ./bin/mmctl user create --local --username $PSH_INITADMIN_USERNAME --email $PSH_INITADMIN_EMAIL --password $PSH_INITADMIN_PASSWORD
@@ -28,15 +26,20 @@ first_deploy() {
     printf "\n  ✔ \033[1mCreating initial channel\033[0m ($PSH_FIRSTCHANNEL_NAME/$PSH_FIRSTCHANNEL_DISPLAYNAME)\n    "
     ./bin/mmctl channel create --local --team $PSH_FIRSTTEAM_NAME --name $PSH_FIRSTCHANNEL_NAME --display-name $PSH_FIRSTCHANNEL_DISPLAYNAME
     printf "\n  ✔ \033[1mPosting welcome/warning messages to channel...\033[0m\n    "
-    ./bin/mmctl post create --local $PSH_FIRSTTEAM_NAME:$PSH_FIRSTCHANNEL_NAME --message $PSH_WELCOME_MESSAGE
+    ./bin/mmctl post create --local $PSH_FIRSTTEAM_NAME:$PSH_FIRSTCHANNEL_NAME --message "$PSH_WELCOME_MESSAGE"
     ./bin/mmctl post create --local $PSH_FIRSTTEAM_NAME:$PSH_FIRSTCHANNEL_NAME --message "$PSH_WARNING_MESSAGE1"
     ./bin/mmctl post create --local $PSH_FIRSTTEAM_NAME:$PSH_FIRSTCHANNEL_NAME --message "$PSH_WARNING_MESSAGE2"
     printf "\n\n\033[1m$PSH_WELCOME_MESSAGE\033[0m"
     printf "\n\033[1m$PSH_WARNING_MESSAGE1\033[0m"
     printf "\n\033[1m$PSH_WARNING_MESSAGE2\033[0m\n\n"
     # Keep track of first deploy.
-    # touch /app/.config/platformsh.installed
+    touch .config/upsun.installed
 }
 
 set_config
 set_local
+
+# Only run first_deploy on initial deployment
+if [ ! -f .config/upsun.installed ]; then
+    first_deploy
+fi
